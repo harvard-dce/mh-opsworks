facbe4d088602e5060f33b3f59a270a107d8cff5
Dan Collis-Puro <dan@collispuro.net>
2015-04-07 10:39:24 -0400

    Check that all base layers are deployed to

---
+++ lib/cluster/config_checks/deployment.rb
@@ -0,0 +1,23 @@
+module Cluster
+  module ConfigChecks
+    class NotAllBaseLayersDeployedTo < StandardError; end
+
+    class Deployment < Base
+      def self.sane?
+        layers = deployment_config.fetch(:to_layers)
+
+        if (base_layers - layers).any?
+          raise NotAllBaseLayersDeployedTo.new(
+            "Some base layers are not configured to be deployed to: #{(base_layers - layers).join(', ')}"
+          )
+        end
+      end
+
+      private
+
+      def self.base_layers
+        ['Admin', 'Engage', 'Workers', 'MySQL db']
+      end
+    end
+  end
+end
---
+++ spec/lib/cluster/config_checks/deployment_spec.rb
@@ -0,0 +1,40 @@
+describe Cluster::ConfigChecks::Deployment do
+  include EnvironmentHelpers
+
+  it 'does not raise when all layers are present' do
+    stub_config_to_include(
+      { stack: deployment_to_layers }
+    )
+
+    expect { described_class.sane? }.not_to raise_error
+  end
+
+  it 'raises when a base layer is not deployed to defaultly' do
+    stub_config_to_include(
+      { stack: deployment_to_layers(['Admin']) }
+    )
+
+    expect { described_class.sane? }.to raise_error(
+      Cluster::ConfigChecks::NotAllBaseLayersDeployedTo
+    )
+  end
+
+  def deployment_to_layers(layers = ["Admin", "Engage", "Workers", "MySQL db"])
+    {
+      app: {
+        shortname: "matterhorn",
+        name: "Matterhorn",
+        type: "other",
+        deployment: {
+          to_layers: layers,
+          custom_json: { }
+        },
+        app_source: {
+          type: "git",
+          url: "git@bitbucket.org:hudcede/matterhorn-dce-fork.git",
+          revision: "master"
+        }
+      }
+    }
+  end
+end
